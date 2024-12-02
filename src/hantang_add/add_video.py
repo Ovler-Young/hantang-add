ec7351b8b8ff3e24a549611328a90380d11b1a2a
Ovler <ovlertheyoung@gmail.com>
2024-12-02 15:25:15 -0500

    feat: Enhance video addition functionality with WBI API integration and finish database part

--- src/hantang_add/add_video.py
+++ src/hantang_add/add_video.py
@@ -1,50 +1,84 @@
 import streamlit as st
 import requests
-
-# Initialize connection.
-conn = st.connection("mysql", type="sql")
+from wbi import encWbi, getWbiKeys
 
 st.header("Add Video")
 
 # input video bv_id
 bv_id = st.text_input("Video BV ID")
 
-st.button("Add Video", key="add_video")
-
-video_info = requests.get(
-    f"https://api.bilibili.com/x/web-interface/view?bvid={bv_id}"
-).json()
-
-# display video info
-
-st.json(video_info)
-
-# Check if the video is in db
-
-current = conn.query(
-    "SELECT * FROM video_static WHERE bvid = %s", (st.session_state.bv_id,)
+st.write(
+    f'<iframe src="//player.bilibili.com/player.html?bvid={bv_id}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>',
+    unsafe_allow_html=True,
 )
 
-if current:
-    st.write("Video already exists in database.")
-else:
-    # try aid
-    aid = video_info["data"]["aid"]
-    current_aid = conn.query("SELECT * FROM video_static WHERE aid = %s", (aid,))
-    if current_aid:
-        st.write("Video already exists in database.")
-    else:
-        add = conn.query(
-            "INSERT INTO video_static (aid, bvid, pubdate, title, description, tag, pic, type_id, user_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (
-                video_info["data"]["aid"],
-                video_info["data"]["bvid"],
-                video_info["data"]["pubdate"],
-                video_info["data"]["title"],
-                video_info["data"]["desc"],
-                "", # todo
-                video_info["data"]["pic"],
-                video_info["data"]["tid"],
-                video_info["data"]["owner"]["mid"],
-            ),
+
+# Get wbi keys
+img_key, sub_key = getWbiKeys()
+# sign params
+signed_params = encWbi(params={"bvid": bv_id}, img_key=img_key, sub_key=sub_key)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Referer": "https://www.bilibili.com/",
+}
+
+response = requests.get(
+    "https://api.bilibili.com/x/web-interface/wbi/view/detail",
+    params=signed_params,
+    headers=headers,
+)
+
+video_info = response.json()
+
+# display video info
+st.header(video_info["data"]["View"]["title"])
+
+video_data = {
+    "aid": video_info["data"]["View"]["aid"],
+    "bvid": video_info["data"]["View"]["bvid"],
+    "pubdate": video_info["data"]["View"]["pubdate"],
+    "title": video_info["data"]["View"]["title"],
+    "description": video_info["data"]["View"]["desc"],
+    "tag": ";".join([tag["tag_name"] for tag in video_info["data"]["Tags"]]),
+    "pic": video_info["data"]["View"]["pic"].replace("http://", "//"),
+    "type_id": video_info["data"]["View"]["tid"],
+    "user_id": video_info["data"]["View"]["owner"]["mid"],
+}
+
+st.table(video_data)
+
+add_video = st.button("Add Video", key="add_video")
+
+if add_video:
+    conn = st.connection("mysql", type="sql")
+
+    query = "SELECT * FROM video_static WHERE aid = '{}' OR bvid = '{}'".format(
+        video_data["aid"], video_data["bvid"]
+    )
+
+    current = conn.query(query, ttl=0)
+
+    if len(current) == 0:
+        query_str = "INSERT INTO video_static (aid, bvid, pubdate, title, description, tag, pic, type_id, user_id) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(
+            video_data["aid"],
+            video_data["bvid"],
+            video_data["pubdate"],
+            video_data["title"],
+            video_data["description"],
+            video_data["tag"],
+            video_data["pic"],
+            video_data["type_id"],
+            video_data["user_id"],
         )
+
+        insert = conn.query(query_str)
+
+        st.success("Video inserted.")
+
+        # Test if the video is in db
+        current = conn.query(query, ttl=0)
+        st.success("Video added successfully.")
+    else:
+        st.success("Video already exists in database.")
+
+    st.table(current.T)
